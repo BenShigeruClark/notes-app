dabad1a91f07a36c64b618c59db6bbfc81ea0347
Ben <benclarkphoto@mac.com>
2019-11-03 19:02:20 -0800

    using validator package

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// Using validator package
+const validator = require('validator')
+const getNotes = require('./notes.js')
 
-const myNote = require('./notes.js')
-
-const msg = myNote()
+const msg = getNotes()
 
 console.log(msg)
+
+console.log(validator.isURL('https:/mead.io'))
 
 
 
