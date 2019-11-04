e2dd516111bcf57b03a474bb145e633bcf2065da
Ben <benclarkphoto@mac.com>
2019-11-03 20:01:49 -0800

    using chalk npm

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-// Using validator package
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
 
-console.log(validator.isURL('https:/mead.io'))
+console.log(chalk.bold.inverse.green('Success!'))
 
+console.log(chalk.bold.inverse.red('Success!'))
 
-
-
-
+console.log(chalk.bold.inverse.yellow('Success!'))
 
